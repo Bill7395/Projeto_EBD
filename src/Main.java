182b94466763389f61554efdc879a3280705f9f3
Bill7395 <gabrielexecutive7395@gmail.com>
2024-05-31 14:05:16 -0300

    Add class "Financeiro" ao projeto.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import br.com.edu.financeiro.Financeiro;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Financeiro financeiro = new Financeiro();
+
+        financeiro.caixa();
     }
 }
+
+/*
+https://github.com/Bill7395/Projeto_EBD.git
+
+Realizado o commit inicial:
+echo "# Projeto_EBD" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Bill7395/Projeto_EBD.git
+git push -u origin main
+ */
---
+++ src/br/com/edu/financeiro/Financeiro.java
@@ -0,0 +1,9 @@
+package br.com.edu.financeiro;
+
+public class Financeiro {
+
+    public void caixa() {
+        System.out.println("Saldo em caixa: R$ 2000.0");
+    }
+
+}
